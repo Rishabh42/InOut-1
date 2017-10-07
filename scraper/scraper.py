b25d4cfac341ab1889f07d4b30fd1f29af55b6c8
Rishabh Thaney <rishabhthaney@gmail.com>
2017-10-08 00:20:25 +0530

    removed quotes from the results (#1)
    * removed quotes from the results
    
    * added code to write output to a txt file

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,12 +6,15 @@ url = 'http://www.prsindia.org/billtrack/industry-commerce-finance/pending'
 html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
 q1 = soup('h3')
-
+tf = open("test.txt","w")
+tf.write("")
+tf.close()
 for tag in q1:
     for i in tag:
-        print i.contents
+        for j in i:
+            j = j[0:]
+            #print j
 
-        #for r in p
-        #    print r.split()
-
-  #get('href', None)
+            with open("test.txt","a") as myfile:
+                x = j.replace(u'\u2019' ,'\n')
+                myfile.write(x+'\n')
